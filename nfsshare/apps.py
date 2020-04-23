d87e3965679b5a3ba24612e3fac0f916a737170b
aahsing <jclahoh@gmail.com>
2020-04-23 16:59:56 +0800

    upload first

---
+++ nfsshare/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class NfsshareConfig(AppConfig):
+    name = 'nfsshare'
---
+++ smbshare/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SmbshareConfig(AppConfig):
+    name = 'smbshare'
